d990f48e62105df1d6388c51a3f4127748763c1a
Hillary Soares <soaresshillary@gmail.com>
2020-02-20 15:58:32 -0300

    calcular area e comprimento circulo

---
+++ Dia4-exercicio5.py
@@ -0,0 +1,10 @@
+try:
+    raio_circulo=float(input('Digite o valor do raio: '))
+    pi=float(3.1416)]
+    area=float((raio_circulo**2)*pi)
+    comprimento=float((raio_circulo*pi)*2)
+    print ('Area: ', area)
+    print ('Comprimento: ', comprimento)
+
+except:
+    print ('Inserir um valor numérico.')
